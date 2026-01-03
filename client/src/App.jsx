6e65d6587b91725d47a0c7895dcb9f9e47aefda8
ZulfRizz <zulfrianwar@gmail.com>
2026-01-03 21:05:02 +0700

    initialize todos app logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect, useEffectEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
+const API_BASE = "http://localhost:3001";
+
 function App() {
-  const [count, setCount] = useState(0)
+  // state : tempat penyimpanan data sementara di browser
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false)
+  const [newTodo, setNewTodo] = useState("")
+
+  // use effect: dijalankan saat pertama kali memuat halaman 
+  useEffect( () => {
+    GetTodos();
+  },[]);
+
+  // ambil data
+  const GetTodos = () => {
+    axios.get(API_BASE, + "/todos")
+      .then(res => setTodos(res.data))
+      .catch(err => console.error("Error:", err))
+  }
+
+  // tambah data
+  const addTodo = async () => {
+    const data = await axios.post(API_BASE + "/todos/new", {
+      text: newTodo
+    });
+
+    setTodos([...todos,data.data])
+    setPopupActive(false)
+    setNewTodo("")
+  }
+
+  // hapus data
+  const deleteData = async (id) => {
+    await axios.delete(API_BASE + "/todos/delete" + id)
+
+    setTodos(todos => todos.filter(todo => todo._id !== id));
+  }
+
+  // toggle complete
+  const completeTodo = async (id) => {
+    const data = await axios.put(API_BASE + "/todos/complete/" + id);
+
+    setTodos(todos => todos.map(todo => {
+      if(todo._id === data.data.id){
+        todo.complete = data.data.complete;
+      }
+      return todo;
+    }));
+  }
+  
 
   return (
     <>
